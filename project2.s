c8a5cfd251f951135797c91439f96e3f3786f3fc
OraneG20 <44150109+OraneG20@users.noreply.github.com>
2018-11-16 21:49:36 -0500

    Getting user input

--- project2.s
+++ project2.s
@@ -3,3 +3,10 @@ input_long: .asciiz "Input is too long."
 invalid_base: .asciiz "Invalid base-31 number."
 empty_input: .asciiz "Input is empty."
 string_input: .space 500000
+.text
+
+main:
+	    
+li $v0, 8   #getting user_input
+la $a0, string_input
+li $a1, 500000
