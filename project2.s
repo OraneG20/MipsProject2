5023a759ab69d234e2441175938a12e9ea94368c
OraneG20 <44150109+OraneG20@users.noreply.github.com>
2018-11-16 22:12:27 -0500

    skip over spaces

--- project2.s
+++ project2.s
@@ -1,6 +1,6 @@
 .data
 input_long: .asciiz "Input is too long."
-invalid_base: .asciiz "Invalid base-31 number."
+invalid_base: .asciiz "Invalid base-35 number."
 empty_input: .asciiz "Input is empty."
 string_input: .space 500000
 .text
@@ -20,6 +20,13 @@ lb $t1,0($t0)
 beq $t1, 10, empty_error #Check for empty input	
 beq $t1, 0 empty_error		
 
-addi $s0, $0, 31 
+addi $s0, $0, 35 
 addi $t4, $0, 1 	
-addi $t5, $0, 0 			
+addi $t5, $0, 0 
+
+move_over_spaces:
+	lb $t1,0($t0)
+	addi $t0, $t0, 1
+	addi $t3, $t3, 1
+	beq $t1, 32, move_over_spaces
+				
